1429697340064ffe921b7bc7dc0bb1779f6a527f
Aditya <khadekaraditya20743@gmail.com>
2023-12-24 11:51:51 +0530

    Database connected

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const connectDB = require('./config/database')
+const express = require('express');
+
+
+connectDB()
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
